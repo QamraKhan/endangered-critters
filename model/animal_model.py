5f554981bd0f0f173820c3b195ad084f0f22af55
Anubhav Bhardwaj <anubhav.bhardwaj@iiitb.org>
2018-09-23 02:30:04 +0800

    adds ._make_predict_function() to remove prediction errors

--- model/animal_model.py
+++ model/animal_model.py
@@ -57,6 +57,7 @@ json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
+loaded_model._make_predict_function()
 # load weights into new model
 loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
