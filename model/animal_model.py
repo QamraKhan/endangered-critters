f9b0844fd420daea88b3f6afbb95920c803bb404
Anubhav Bhardwaj <anubhav.bhardwaj@iiitb.org>
2018-09-23 03:10:28 +0800

    adds models for 10 animals classification and removes dogs

--- model/animal_model.py
+++ model/animal_model.py
@@ -53,137 +53,27 @@ HEIGHT=400
 WIDTH=400
 VAL_SPLIT = 0.2 #Percentage of training examples used for validation
 
-json_file = open('model.json', 'r')
+json_file = open('model_10_animal.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 loaded_model._make_predict_function()
 # load weights into new model
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("model_10_animals.h5")
 print("Loaded model from disk")
 
-logreg = pickle.load(open('abc.model', 'rb'))
+logreg = pickle.load(open('logreg_10_animals.model', 'rb'))
 
-dogs_list = ['affenpinscher',
- 'afghan_hound',
- 'african_hunting_dog',
- 'airedale',
- 'american_staffordshire_terrier',
- 'appenzeller',
- 'australian_terrier',
- 'basenji',
- 'basset',
- 'beagle',
- 'bedlington_terrier',
- 'bernese_mountain_dog',
- 'black-and-tan_coonhound',
- 'blenheim_spaniel',
- 'bloodhound',
- 'bluetick',
- 'border_collie',
- 'border_terrier',
- 'borzoi',
- 'boston_bull',
- 'bouvier_des_flandres',
- 'boxer',
- 'brabancon_griffon',
- 'briard',
- 'brittany_spaniel',
- 'bull_mastiff',
- 'cairn',
- 'cardigan',
- 'chesapeake_bay_retriever',
+animals_list = ['buffalo',
  'chihuahua',
- 'chow',
- 'clumber',
- 'cocker_spaniel',
- 'collie',
- 'curly-coated_retriever',
- 'dandie_dinmont',
- 'dhole',
- 'dingo',
- 'doberman',
- 'english_foxhound',
- 'english_setter',
- 'english_springer',
- 'entlebucher',
- 'eskimo_dog',
- 'flat-coated_retriever',
- 'french_bulldog',
- 'german_shepherd',
- 'german_short-haired_pointer',
- 'giant_schnauzer',
- 'golden_retriever',
- 'gordon_setter',
- 'great_dane',
- 'great_pyrenees',
- 'greater_swiss_mountain_dog',
- 'groenendael',
- 'ibizan_hound',
- 'irish_setter',
- 'irish_terrier',
- 'irish_water_spaniel',
- 'irish_wolfhound',
- 'italian_greyhound',
- 'japanese_spaniel',
- 'keeshond',
- 'kelpie',
- 'kerry_blue_terrier',
- 'komondor',
- 'kuvasz',
- 'labrador_retriever',
- 'lakeland_terrier',
- 'leonberg',
- 'lhasa',
- 'malamute',
- 'malinois',
- 'maltese_dog',
- 'mexican_hairless',
- 'miniature_pinscher',
- 'miniature_poodle',
- 'miniature_schnauzer',
- 'newfoundland',
- 'norfolk_terrier',
- 'norwegian_elkhound',
- 'norwich_terrier',
- 'old_english_sheepdog',
- 'otterhound',
- 'papillon',
- 'pekinese',
- 'pembroke',
- 'pomeranian',
- 'pug',
- 'redbone',
- 'rhodesian_ridgeback',
- 'rottweiler',
- 'saint_bernard',
- 'saluki',
- 'samoyed',
- 'schipperke',
- 'scotch_terrier',
- 'scottish_deerhound',
- 'sealyham_terrier',
- 'shetland_sheepdog',
- 'shih-tzu',
- 'siberian_husky',
- 'silky_terrier',
- 'soft-coated_wheaten_terrier',
- 'staffordshire_bullterrier',
- 'standard_poodle',
- 'standard_schnauzer',
- 'sussex_spaniel',
- 'tibetan_mastiff',
- 'tibetan_terrier',
- 'toy_poodle',
- 'toy_terrier',
- 'vizsla',
- 'walker_hound',
- 'weimaraner',
- 'welsh_springer_spaniel',
- 'west_highland_white_terrier',
- 'whippet',
- 'wire-haired_fox_terrier',
- 'yorkshire_terrier']
+ 'chimpanzee',
+ 'elephant',
+ 'giant+panda',
+ 'grizzly+bear',
+ 'hippopotamus',
+ 'horse',
+ 'lion',
+ 'tiger']
 
 
 def read_img(img_path,size):
@@ -198,6 +88,6 @@ def predict(image_path):
     
     test_x_bf = loaded_model.predict(x_train, batch_size=32, verbose=1)
     print (logreg.predict(test_x_bf)[0])
-    return dogs_list[logreg.predict(test_x_bf)[0]]
+    return animals_list[logreg.predict(test_x_bf)[0]]
 
     
